a01e580d62ee0e2b715282fc3daf6248acf1f36f
vv2529 <vv02529@gmail.com>
2021-03-08 21:01:08 +0200

    Implement basic UI functionality

---
+++ js/main.js
@@ -0,0 +1,46 @@
+document.getElementsByClassName('card-wrapper')[0].addEventListener('keypress', e => {
+	const elem = e.target;
+	if (e.keyCode == 13) {
+		if (elem.classList.contains('option')) {
+			elem.click();
+		} else {
+			document.getElementsByClassName('btn-options')[0].click();
+		}
+	}
+});
+
+document.getElementsByClassName('card-wrapper')[0].addEventListener('click', e => {
+	e.stopPropagation();
+	const elem = e.target;
+	console.log(elem);
+	if (elem.id == 'option-delete') {
+		elem.parentElement.parentElement.parentElement.remove();
+	}
+});
+
+document.body.addEventListener('click', e => {
+	const inputs = document.getElementsByClassName('options-checkbox');
+	for (let i = 0; i < inputs.length; i++) {
+		console.log(inputs[i]);
+		inputs[i].checked = false;
+	}
+});
+
+document.getElementById('btn-add').addEventListener('click', e => {
+	document.getElementById('form-wrapper').classList.remove('hidden');
+});
+
+document.getElementById('btn-close').addEventListener('click', e => {
+	const elem = e.target;
+	elem.parentElement.parentElement.classList.add('hidden');
+});
+
+document.getElementById('btn-close').addEventListener('keypress', e => {
+	if (e.keyCode == 13) {
+		document.getElementById('btn-close').click();
+	}
+});
+
+document.getElementsByClassName('option-delete')[0].addEventListener('blur', e => {
+	document.getElementsByClassName('btn-options')[0].click();
+});
