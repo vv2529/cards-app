64d3690a433b836cc027d359c50f80a3885cd4ac
vv2529 <vv02529@gmail.com>
2021-03-09 23:17:32 +0200

    Fix relative paths

--- js/loading.js
+++ js/loading.js
@@ -1,22 +1,22 @@
 function downloadCards(callback) {
-	fetch('../php/download.php').
+	fetch('php/download.php').
 		then(response => response.json()).
 		then(cards => callback(cards)).
 		catch(() => {
-			fetch('../json/example.json').
+			fetch('json/example.json').
 			then(response => response.json()).
 			then(cards => callback(cards));
 		});
 }
 
 function uploadCards({ name, dob, gender }, callback) {
-	fetch(encodeURI(`../php/upload.php?name=${name}&dob=${dob}&gender=${gender}`)).
+	fetch(encodeURI(`php/upload.php?name=${name}&dob=${dob}&gender=${gender}`)).
 		then(response => response.json()).
 		then(card => callback(card));
 }
 
 function deleteCard(id) {
-	fetch(`../php/delete.php?id=${id}`);
+	fetch(`php/delete.php?id=${id}`);
 }
 
 export { downloadCards, uploadCards, deleteCard };
